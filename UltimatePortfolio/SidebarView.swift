8bff404ff2292177643e7ea1052d8dfb7af628a2
Dang Hoang <danghmh1205@gmail.com>
2025-10-18 21:14:52 +0700

    feat(ui): build sidebar with smart filters and user tags

--- UltimatePortfolio/SidebarView.swift
+++ UltimatePortfolio/SidebarView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct SidebarView: View {
+    @Environment(DataController.self) var dataController
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.name)]) var tags: FetchedResults<Tag>
+    
+    let smartFilters: [Filter] = [.all, .recent]
+    var tagFilters: [Filter] {
+        tags.map { tag in
+            Filter(id: tag.id ?? UUID(), name: tag.name ?? "No name", icon: "tag", tag: tag)
+        }
+    }
+    
+    
     var body: some View {
-        Text("Sidebar")
+        List(selection: Bindable(dataController).selectedFilter) {
+            Section("Smart Filters") {
+                ForEach(smartFilters) { filter in
+                    NavigationLink(value: filter) {
+                        Label(filter.name, systemImage: filter.icon)
+                    }
+                }
+            }
+            
+            Section("Tags") {
+                ForEach(tagFilters) { filter in
+                    NavigationLink(value: filter) {
+                        Label(filter.name, systemImage: filter.icon)
+                    }
+                }
+            }
+        }
+        .toolbar {
+            Button {
+                dataController.deleteAll()
+                dataController.createSampleData()
+            } label: {
+                Label("ADD SAMPLES", systemImage: "flame")
+            }
+        }
     }
 }
 
 #Preview {
     SidebarView()
+        .environment(DataController.preview)
 }
