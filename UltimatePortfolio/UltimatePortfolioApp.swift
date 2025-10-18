b10bc901a2e81daeaa7ede94ca24b638d94e29e6
Dang Hoang <danghmh1205@gmail.com>
2025-10-18 19:36:12 +0700

    initial project setup

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Hoàng Minh Hải Đăng on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
